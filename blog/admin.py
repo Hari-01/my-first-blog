9b0f22d0b567c8189f38a1f0e2c4c12caf375010
hari <f20190564@pilani.bits-pilani.ac.in>
2020-07-09 11:54:00 +0530

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+# to dd,edit and delete the posts we have just modelled we use Django amin
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
+
+# Register your models here.
